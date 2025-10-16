ff85faff00a547187f8e1b1a7da2cdee8ad0fc7e
Bharat Kumar <bharat.nanavathula@gmail.com>
2025-10-16 11:58:49 +0530

    a

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ eslint: {
+    ignoreDuringBuilds: true, // ⬅️ disables ESLint in build
+  },
+  typescript: {
+    ignoreBuildErrors: true,  // ⬅️ (optional) lets TS errors pass build
+  },
 };
 
 export default nextConfig;
